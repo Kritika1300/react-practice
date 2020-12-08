939aa3973f3056e6c0886e586e68d577e9a68795
Kritika1300 <62663065+Kritika1300@users.noreply.github.com>
2020-12-08 10:34:00 +0530

    Handling Events

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import React,{useState} from 'react';
 import './App.css';
 const App = () => {
-let newTime = new Date().toLocaleTimeString();
-const [ctime,setTime] = useState(newTime);
-const updateTime = () => {
-  newTime = new Date().toLocaleTimeString();
-  setTime(newTime);
-};
-setInterval(updateTime,1000);
-  return (
-    <>
-    <h1>{ctime}</h1>
-    </>
-  );
-}
+const colour = "red";
+const [bg,setBg] = useState(colour);
+const [name,setName] = useState('Hello');
+const bgChange = () => {
+ let newBg = 'black';
+ setBg(newBg);
+ setName('Kritika');
 
+};
+    return(
+      <>
+      <div style = {{backgroundColor : bg}}>
+      <button onClick = {bgChange}> {name} </button>
+      </div>
+      </>
+
+    );
+};
 export default App;
